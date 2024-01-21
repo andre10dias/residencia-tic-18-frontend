d646fb7c5ac7500631cb92521a9e6cecf737f6fe
andre10dias <andre10dias@ymail.com>
2024-01-21 20:27:14 -0300

    criando Diretivas

--- JReader/src/app/app.module.ts
+++ JReader/src/app/app.module.ts
@@ -20,6 +20,8 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
+import { TituloDirective } from './titulo.directive';
+import { ConteudoDirective } from './conteudo.directive';
 
 @NgModule({
   declarations: [
@@ -32,7 +34,9 @@ import {MatCardModule} from '@angular/material/card';
     CategoriaComponent,
     DetalhaAviaoComponent,
     DetalhaCarroComponent,
-    DetalhaBarcoComponent
+    DetalhaBarcoComponent,
+    TituloDirective,
+    ConteudoDirective
   ],
   imports: [
     BrowserModule,
