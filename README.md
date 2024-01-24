0c71045fe4b6c5cceb223a15c6f1a050407a7b50
andre10dias <andre10dias@ymail.com>
2024-01-24 12:00:46 -0300

    Iniciando projeto

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 ## Carlos André Dias
 
 ### Repositório das atividades de Instrução Prática Frontend
+
+
+```
+ng new minhaApp --no-standalone --routing --ssr=false
+```
+```
+ng add @angular/material
+```
---
+++ wikiApp/src/app/resultado-busca/resultado-busca.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resultado-busca',
+  templateUrl: './resultado-busca.component.html',
+  styleUrl: './resultado-busca.component.css'
+})
+export class ResultadoBuscaComponent {
+
+}
