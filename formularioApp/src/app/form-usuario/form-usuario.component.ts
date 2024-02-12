e3146b835c27ee674adf5017f1f9db54a7a544ce
andre10dias <andre10dias@ymail.com>
2024-02-12 17:11:11 -0300

    Validações

--- formularioApp/src/app/form-usuario/form-usuario.component.ts
+++ formularioApp/src/app/form-usuario/form-usuario.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, SimpleChanges } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { max } from 'rxjs';
 
 @Component({
   selector: 'app-form-usuario',
@@ -12,49 +13,170 @@ export class FormUsuarioComponent {
 
   hide = true;
   generos: any[] = [
-    {value: '', viewValue: 'Selecione...'},
     {value: 'M', viewValue: 'Masculino'},
     {value: 'F', viewValue: 'Feminino'},
   ];
+  profissoes: any[] = [
+    {value: 'analista', viewValue: 'Analista de sistemas'},
+    {value: 'engenheiro', viewValue: 'Engenheiro'},
+    {value: 'professor', viewValue: 'Professor'},
+  ]
+  buttonDisabled: boolean = true;
 
   today: Date = new Date();
   day = this.today.getDay();
   month = this.today.getMonth();
   year = this.today.getFullYear();
-
+  
   constructor() {
+    this.bindValidator();
+
+    // const profissaoControls: { [key: string]: FormControl } = {};
+    // this.profissoes.forEach((profissao: any) => {
+    //   profissaoControls[profissao.value] = new FormControl(false);
+    // });
+
+    const profissaoControls: any[] = [];
+    this.profissoes.forEach((profissao: any) => {
+      profissaoControls[profissao.value] = new FormControl(false);
+    });
+
+    this.profissao = new FormGroup(profissaoControls, this.profissaoValidator);
+
     this.form = new FormGroup({
-      'nomeUsuario': new FormControl(null, Validators.required),
-      'senha': new FormControl(null, Validators.required),
+      'nomeUsuario': new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(12),
+        this.nomeUsuarioValidator
+      ]),
+      'senha': new FormControl(null, [
+        Validators.required,
+        Validators.minLength(4),
+        this.senhaValidator
+      ]),
       'email': new FormControl(null, [
         Validators.required,
         Validators.email
       ]),
-      'nomeCompleto': new FormControl(null, Validators.required),
-      'telefone': new FormControl(null, Validators.required),
+      'nomeCompleto': new FormControl(null, [
+        Validators.required,
+        this.nomeCompletoValidator
+      ]),
+      'telefone': new FormControl(null, [
+        Validators.required,
+        Validators.minLength(11),
+        Validators.maxLength(11)
+      ]),
       'endereco': new FormControl(null, Validators.required),
-      'nascimento': new FormControl(null),
-      'genero': new FormControl(null, Validators.required)
+      'nascimento': new FormControl(null, [
+        Validators.required,
+        this.idadeMinimaValidator
+      ]),
+      'genero': new FormControl(null, Validators.required),
+      'profissao': this.profissao
+    });
+    
+    this.form.statusChanges.subscribe(() => {
+      this.buttonDisabled = !this.form.valid;
     });
 
-    this.profissao = new FormGroup({
-      'analistaSistemas': new FormControl(false),
-      'engenheiro': new FormControl(false),
-      'professor': new FormControl(false)
-    });
   }
 
-  getErrorMessage() {
-    // if (this.email.hasError('required')) {
-    //   return 'You must enter a value';
-    // }
+  nomeUsuarioValidator(control: AbstractControl): { [key: string]: boolean } | null {
+    return this.fieldValidator(control, /^\S+$/);
+  }
 
-    // return this.email.hasError('email') ? 'Not a valid email' : '';
+  senhaValidator(control: AbstractControl): { [key: string]: boolean } | null {
+    return this.fieldValidator(control, /(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.{4,})/);
+  }
+
+  nomeCompletoValidator(control: AbstractControl): { [key: string]: boolean } | null {
+    return this.fieldValidator(control, /^[a-zA-Z]+\s[a-zA-Z]+$/);
+  }
+
+  idadeMinimaValidator(control: AbstractControl): { [key: string]: boolean } | null {
+    const dataNascimento = new Date(control.value);
+    const hoje = new Date();
+    const idade = hoje.getFullYear() - dataNascimento.getFullYear();
+
+    if (idade < 18) {
+      return { idadeMinimaInvalida: true };
+    }
+
+    return null;
+  }
+
+//   verificarProfissaoSelecionada(): boolean {
+//   for (const profissao of this.profissoes) {
+//     if (this.profissao.get(profissao.value)?.value) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+
+// ngOnInit() {
+//   this.form.valueChanges.subscribe(() => {
+//     this.buttonDisabled = !this.form.valid || !this.verificarProfissaoSelecionada();
+//   });
+// }
+
+
+  profissaoValidator(control: AbstractControl): { [key: string]: boolean } | null {
+    let isValid = null;
+
+    this.profissoes?.forEach((element: any) => {
+      let profissao = control.get(element.value)?.value;
+
+      console.log('Profissao: ', profissao);
+
+      if (profissao && this.form.valid) {
+        isValid = true;
+      }
+    });
+
+    if (!isValid) {
+      this.buttonDisabled = true;
+    }
+
+    return null;
+  }
+
+  formatarTelefone(): string {
+    let telefone = this.form.get('telefone')?.value;
+
+    if (telefone != null) {
+      // Remove todos os caracteres não numéricos do número de telefone
+      const numerosTelefone = telefone.replace(/\D/g, '');
+  
+      // Aplica a máscara ao número de telefone
+      const telefoneFormatado = numerosTelefone.replace(/^(\d{2})(\d{1})(\d{4})(\d{4})$/, '($1) $2 $3-$4');
+      return telefoneFormatado;
+    }
+
+    return telefone;
   }
 
   onSubmit() {
     // console.log('form: ', this.form);
-    this.form.value.profissao = this.profissao.value;
+    // this.form.value.profissao = this.profissao.value;
     console.log(this.form.value);
   }
+
+  fieldValidator(control: AbstractControl, regex: RegExp): { [key: string]: boolean } | null {
+    if (!regex.test(control.value)) {
+      return { invalid: true };
+    }
+
+    return null;
+  }
+
+  // Vinculando o contexto de 'this' às funções de validação:
+  bindValidator() {
+    this.nomeUsuarioValidator = this.nomeUsuarioValidator.bind(this);
+    this.senhaValidator = this.senhaValidator.bind(this);
+    this.nomeCompletoValidator = this.nomeCompletoValidator.bind(this);
+    this.profissaoValidator = this.profissaoValidator.bind(this);
+  }
+
 }
